4a1f97c4296080ea47757300a27f6eaf0fa64b02
Vincent <vinc3r@MacBook-Pro.local>
2015-03-20 08:35:56 +0100

    removed dir

--- hid/attacks/winsys/shell.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for i in `ls -x *hid`;do
-	cat $i > /dev/hidg0
-	sleep 2
-done
--- hid/shell.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cat open_hid > /dev/hidg0
-cat terminal_hid > /dev/hidg0
-cat enter > /dev/hidg0
-cat commands_hid > /dev/hidg0
-cat enter > /dev/hidg0
